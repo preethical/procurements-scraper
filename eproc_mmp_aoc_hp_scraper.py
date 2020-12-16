82101c5b897e67880543b3efe0bac058e0173420
preethical <preethi@deobald.ca>
2020-12-16 11:07:45 -0600

    initial commit

---
+++ eproc_mmp_aoc_hp_scraper.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+'Class for scrapping eProcurement MMP Portal for HP Result of Tenders'
+
+import argparse
+import csv
+import datetime
+from lxml import etree
+import logging
+from logging.config import fileConfig
+import os
+import re
+from selenium_scraper_utils import SeleniumScrappingUtils
+import sys, traceback
+import time
+
+OUT_HEADER = ["S.No.", "AOC Date", "e-Published Date", "Title and Ref.No./Tender Id", "State Name", "Tender ID", "Organisation Name", "Tender Ref. No.", "Tender Description", "Tender Type", "Number of bids received", "Name of the selected bidder(s)", "Address of the selected bidder(s)", "Contract Value", "Tender Published Date", "Contract Date", "Date of Completion/Completion Period in Days"]
+BASE_URL = "https://eprocure.gov.in/mmp/resultoftenders"
+SLEEP_TIME = 2
+
+class EprocMmpAocHPscraper(SeleniumScrappingUtils):
+
+    def get_tender_results(self, output_file):
+        tender_results = []
+        csv_writer,out_csv_file = self.get_file_writer(output_file)
+        page_dom = self.get_page_dom(url=BASE_URL)
+        state_elem = self.get_page_element(xpath='//select[@name="org_name"]')
+        #TODO CAUTION
+        #self.select_element(state_elem, text="Himachal Pradesh")
+        #time.sleep(20) #Enter CAPTCHA
+        #submit_button = self.get_page_element(xpath='//input[@value="Search"]')
+        #submit_button.click()
+        #time.sleep(SLEEP_TIME)
+        page_dom = self.get_page_dom() 
+        #TODO CAUTION
+        url = self.driver.current_url
+        url = url + "/page=6750"
+        page_dom = self.get_page_dom(url)
+        while True:
+            tender_records = self.get_tender_records_from_page(page_dom) 
+            self.write_tender_details(tender_records, csv_writer)
+            print(tender_records)
+            time.sleep(SLEEP_TIME)
+            previous_button = self.get_page_element(xpath="//a[@class='page_parination' and contains(.,'Previous')]")
+            if not previous_button:
+                break
+            previous_button.click()
+            time.sleep(SLEEP_TIME)
+            page_dom = self.get_page_dom() 
+        out_csv_file.close()
+
+    def get_tender_records_from_page(self, page_dom):
+        record_list = []
+        rows = page_dom.xpath("//div[@id='edit-l-result-teners']/table[@id='table']//tr")
+        for row_number in range(len(rows)):
+            if row_number == 0:
+                continue #Skipping Empty Row
+            row = rows[row_number]
+            records = row.xpath(".//td//text()")
+            records.remove(' ')
+            tender_id = records[3].split(" / ")[-1].strip()
+            records.append(tender_id)
+            record_link = self.get_page_element(xpath=".//tr[%s]//td[4]/a" % format(row_number+1))
+            if record_link:
+                record_link.click()
+                page_dom = self.get_page_dom()
+                records_details = self.get_tender_details(page_dom)
+                records += records_details
+                back_link = self.get_page_element(xpath="//form[@id='aocfullview-form']//a[1]") 
+                back_link.click()
+            print(records)
+            record_list.append(records)
+        return record_list
+
+    def get_tender_details(self, page_dom):
+        tender_details = page_dom.xpath("//td[@class='td_view_field']//text()")
+        return tender_details
+
+    def get_file_writer(self, output_file):
+        out_csv_file = open(output_file, "w")
+        csv_writer = csv.writer(out_csv_file, delimiter=',')
+        csv_writer.writerow(OUT_HEADER)
+        return csv_writer, out_csv_file
+    
+    def write_tender_details(self, tender_records, csv_writer):
+        for row in tender_records:
+            encoded_row = self.encode_row(row)
+            csv_writer.writerow(encoded_row)
+        return csv_writer
+
+    def encode_row(self, row):
+        encoded_row = []
+        for item in row:
+            item = item.encode('utf-8')
+            encoded_row.append(item)
+        return encoded_row
+
+if __name__ == '__main__':
+    obj = EprocMmpAocHPscraper()
+    obj.get_tender_results("aoc_all_records2.csv")     
---
+++ selenium_scraper_utils.py
@@ -0,0 +1,78 @@
+'Class for Selenium Scrapping utility functions'
+
+import logging
+from logging.config import fileConfig
+from lxml import etree
+import os
+import re
+import requests
+from requests.adapters import HTTPAdapter
+import time
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import shutil
+from urllib3.util import Retry
+
+DEFAULT_XPATH = ".//text()"
+MAX_URL_RETRIES = 25
+SAMPLE_URL = "https://www.civicdatalab.in/"
+TEXT_DOC_XPATH = "//text()"
+SLEEP_TIME = 60
+MAX_RELOADS = 3
+
+class SeleniumScrappingUtils(object):
+    def __init__(self):
+        '''Initializes session for scrapping utils
+        '''
+        self.retry_status_codes = [404,500,502,503,504] 
+        self.driver = webdriver.Firefox() 
+        self.driver.get(SAMPLE_URL)
+
+    def get_page_from_url(self, url):
+        '''Fetches URL and return its textual content, in case of error returns empty text
+        '''
+        page_text = ''
+        self.driver.get(url)
+        time.sleep(1)
+        page_text = self.driver.page_source
+        return page_text
+
+    def get_page_dom(self, url=None):
+        '''Returns dom tree of the driver's page source
+        '''
+        dom_tree = None
+        if url:
+            page_text = self.get_page_from_url(url)
+        else:
+            time.sleep(1)
+            page_text = self.driver.page_source
+        if page_text:
+            dom_tree = etree.HTML(page_text)
+        return dom_tree
+
+    def get_page_element(self, xpath=None):
+        ''' Get page element by xpath
+        '''
+        page_element = None
+        reload_attempts = 0
+        while not page_element and reload_attempts < MAX_RELOADS:
+            try:
+                page_element = WebDriverWait(self.driver, SLEEP_TIME).until(
+                    EC.presence_of_element_located((By.XPATH, xpath))
+                )
+            except NoSuchElementException as error_message:
+                print(error_message)
+            except Exception as error_message:
+                print(error_message)
+            if not page_element:
+                self.driver.refresh()
+            reload_attempts += 1 
+        return page_element
+    
+    def select_element(self, select_element, text=None):
+        select = Select(select_element)
+        select.select_by_visible_text(text)
